230a1359554024954b965c1c2ed68a9db668a6d0
Sam Puttman <daputter2@gmail.com>
2024-05-09 10:38:07 -0500

    Create subscription facade, update controller to utilize it, along with some private methods to set customer and subscription

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,32 +1,47 @@
 class Api::V1::SubscriptionsController < ApplicationController
+  before_action :set_customer, only: [:index, :create, :update, :destroy]
+  before_action :set_subscription, only: [:update, :destroy]
 
   def index
-    customer = CustomerFacade.find_customer(params[:customer_id])
-    subscriptions = customer.subscriptions.map { |subscription| Subscription.new(subscription.attributes) }
-    render json: subscriptions
+    subscriptions = SubscriptionFacade.find_subscriptions(@customer)
+    render json: subscriptions.as_json(include: :teas, methods: :price)
   end
 
   def create
-    customer = CustomerFacade.find_customer(params[:customer_id])
-    subscription = customer.subscriptions.create(subscription_params)
-    if subscription.persisted?
-      render json: Subscription.new(subscription.attributes), status: :created
+    subscription = @customer.subscriptions.build(subscription_params)
+
+    if subscription.save
+      render json: SubscriptionSerializer.new(subscription).serializable_hash, status: :created
     else
-      render json: { errors: subscription.errors }, status: :unprocessable_entity
+      render json: { errors: subscription.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+  
+  def update
+    subscription = SubscriptionFacade.update_subscription(@subscription, subscription_params, params[:subscription][:tea_ids])
+    if subscription.valid?
+      render json: SubscriptionSerializer.new(subscription).serializable_hash
+    else
+      render json: { errors: subscription.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    customer = CustomerFacade.find_customer(params[:customer_id])
-    subscription = customer.subscriptions.find(params[:id])
-    subscription.destroy
+    @subscription.destroy
     head :no_content
   end
 
   private
 
-  def subscription_params
-    params.require(:subscription).permit(:title, :price, :status, :frequency)
+  def set_customer
+    @customer = CustomerFacade.find_customer(params[:customer_id])
   end
 
+  def set_subscription
+    @subscription = @customer.subscriptions.find(params[:id])
+  end
+
+  def subscription_params
+    params.require(:subscription).permit(:title, :status, :frequency, subscription_teas_attributes: [:tea_id, :quantity])
+  end
 end
